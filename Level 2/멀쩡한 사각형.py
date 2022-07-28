80af8bb17c259ef55bb8bd1fd80c1cb627ab2672
choi sungmin <89989648+choisony@users.noreply.github.com>
2022-07-28 15:42:43 +0900

    Create 멀쩡한 사각형.py

---
+++ Level 2/멀쩡한 사각형.py
@@ -0,0 +1,13 @@
+def gcd(w,h):
+    if h ==0:
+        return w
+    else:
+        return gcd(h,w%h)
+    
+    
+def solution(w,h):
+    if h>w:
+        w,h = h,w
+    g = gcd(w,h)
+    
+    return w*h-(w+h-g)
