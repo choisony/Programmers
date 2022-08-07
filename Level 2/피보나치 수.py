9e251ee6109e5c2a7d1ae11539d73c01c13f9c18
choi sungmin <89989648+choisony@users.noreply.github.com>
2022-08-07 22:01:40 +0900

    O

---
+++ Level 2/피보나치 수.py
@@ -0,0 +1,10 @@
+https://school.programmers.co.kr/learn/courses/30/lessons/12945
+  
+def solution(n):
+    dp=[0]* (n+1)
+    dp[0] = 0
+    dp[1] = 1
+    for i in range(2,n+1):
+        dp[i] = dp[i-2]+dp[i-1]
+    answer= dp[n] % 1234567
+    return answer
