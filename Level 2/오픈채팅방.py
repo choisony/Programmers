093bcb091acee40116001e800d33cc4a7f83c7de
choi sungmin <89989648+choisony@users.noreply.github.com>
2022-08-16 19:08:27 +0900

    O

---
+++ Level 2/오픈채팅방.py
@@ -0,0 +1,28 @@
+https://school.programmers.co.kr/learn/courses/30/lessons/42888
+
+def solution(record):
+    d={}
+    result = []
+    for i in record:
+        temp= i.split()
+        if temp[0]=="Enter":
+            result.append("님이 들어왔습니다.")
+            d[temp[1]] = temp[2]
+        elif temp[0] =="Leave":
+            result.append("님이 나갔습니다.")
+        elif temp[0]=="Change":
+            d[temp[1]] = temp[2]
+    
+    idx = 0
+    for i in record:
+        temp = i.split()
+        if temp[0]=="Enter":
+            result[idx] = d[temp[1]]+ result[idx]
+            idx+=1
+        if temp[0] =="Leave":
+            result[idx] = d[temp[1]]+ result[idx]
+            idx+=1
+
+        
+    
+    return result
